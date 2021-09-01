19afde2f7b468e9d8d254af568e37f21c48e59bf
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-09-02 03:53:00 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const dataLoad = async () => {
 
     } else {
         document.getElementById("error").style.display = "none"
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `HTTPS://openlibrary.org/search.json?q=${searchText}`;
         const res = await fetch(url);
         const data = await res.json();
         displayResult(data.docs)
@@ -35,22 +35,23 @@ const displayResult = (books) => {
         const searchResult = document.getElementById("search-result");
         searchResult.textContent = '';
         document.getElementById("search-founded").style.display = "block"
-        document.getElementById("result-count").innerText = `Your search result is ${books.length}`
+        document.getElementById("result-count").innerText = `Your search result is: ${books.length}`
         books.forEach(book => {
 
             const div = document.createElement("div");
             div.classList.add("col");
             div.innerHTML = `
-             <div class="card">
-             <h5 class="card-title">Book Name: ${book.text[1] ? book.text[1] : ''}</h5>
-             <h6>Author Name: ${book.author_name ? book.author_name :''}</h6>
-             <p>First publish: ${book.first_publish_year ? book.first_publish_year :'' } </p>
+             <div class="card align-items-center">
+                <img src="HTTPS://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 py-3" alt="...">
+                <h6 class="card-title text-bold"><strong>Book Name:</strong> ${book.title ? book.title : ''}</h6>
+                <p class="mb-0"><strong>Author Name:</strong> ${book.author_name ? book.author_name :''}</p>
+                <small>First publish: ${book.first_publish_year ? book.first_publish_year :'' } </small>
              </div>
              `
             searchResult.appendChild(div)
 
 
-            // console.log(book)
+            console.log(book)
         })
     }
 
