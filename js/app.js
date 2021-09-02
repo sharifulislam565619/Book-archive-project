c1f53bb91f8ab97c4a6bd82b54a1042ec8537a96
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-09-02 12:56:53 +0600

    some text added

--- js/app.js
+++ js/app.js
@@ -14,44 +14,52 @@ const dataLoad = async () => {
         const url = `HTTPS://openlibrary.org/search.json?q=${searchText}`;
         const res = await fetch(url);
         const data = await res.json();
-        displayResult(data.docs)
+        // console.log(data)
+        displayResult(data)
     }
 
 
 
 }
 
-// spinner
+// Loading spinner
 const toggleSpinner = (spinning) => {
     document.getElementById("spinner").style.display = `${spinning}`
 }
 
-// not found
+// Result not found
 const searchNotFount = (display) => {
     document.getElementById("search-founded").style.display = `${display}`
 
 }
 
+// total result count
+const resultCount=(display)=>{
+    document.getElementById("result-count").style.display=`${display}`
+}
+
 // display result
-const displayResult = (books) => {
+const displayResult = (data) => {
+    const books = data.docs
     const searchResult = document.getElementById("search-result");
     searchResult.textContent = '';
     searchNotFount("none")
-
-
-    if (books.length === 0) {
+ 
+    if (data.numFound === 0) {
+        resultCount( "none");
         searchNotFount("block")
     } else {
 
-        searchNotFount("block")
-        document.getElementById("result-count").innerText = `Your search result is: ${books.length}`
+        searchNotFount("none")
+        resultCount( "block");
+        document.getElementById("result-count").innerText = `Your search result is: ${data.numFound}`
         books.forEach(book => {
 
             const div = document.createElement("div");
             div.classList.add("col");
             div.innerHTML = `
              <div class="card align-items-center">
-                <img src="HTTPS://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 py-3" alt="...">
+                <img src="HTTPS://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 py-3" alt="Image not found">
                 <h6 class="card-title text-bold"><strong>Book Name:</strong> ${book.title ? book.title : ''}</h6>
                 <p class="mb-0"><strong>Author Name:</strong> ${book.author_name ? book.author_name :''}</p>
                 <small>First publish: ${book.first_publish_year ? book.first_publish_year :'' } </small>
